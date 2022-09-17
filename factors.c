b094cdbed34249c0e3ddc25c30a9ad46259aba97
nefelibatala1993 <leuela1993@gmail.com>
2022-09-17 20:43:08 +0300

    Added Files

--- factors.c
+++ factors.c
@@ -1,30 +1,76 @@
-#include "rsa.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <string.h>
+#include <gmp.h>
+
+void file_input(char *filename);
+void factors(mpz_t n);
 
 /**
- * main - Entry point for my program
- * @ac: The number of command line arguments
- * @av: An array that holds the command line arguments as
- * string a string array
- *
- * Return: On success, it returns 0. On faliure, it returns
- * -1.
+ * file_input - read from file
+ * @filename: file to read from
+ * Return: line
  */
-int main(int ac, char **av)
-{
-	char **num_str;
-	int n = 0, i;
-	mpz_t num_from_str;
 
-	if (ac != 2)
+void file_input(char *filename)
+{
+	char *line = NULL;
+	mpz_t bsize = 0;
+	mpz_t line_to_num;
+	FILE *fp;
+
+	if (filename == NULL)
+		return;
+
+	fp = fopen(filename, "r");
+	if (fp == NULL)
+		return;
+
+	while (getline(&line, &bsize, fp) != -1)
 	{
-		fprintf(stderr, "Usage: ./factors <file_name>\n");
-		exit(EXIT_FAILURE);
+		mpz_init_set_str(line_to_num, line, 10);
+		factors(line_to_num);
 	}
-	num_str = read_file(av[1], &n);
-	for (i = 0; i < n; i++)
+
+	fclose(fp);
+}
+
+
+/**
+ * factors - factors the nums
+ * @n: number to factor
+ * Return: VOID
+ */
+
+void factors(mpz_t n)
+{
+	mpz_t i;
+
+	for (i = 2; i < n; i++)
 	{
-		mpz_set_str(num_from_str, num_str[i], 10);
-		find_factor(num_from_str);
+		if (n == ((n / i) * i))
+		{
+			printf("%ld=%ld*%ld\n", n, (n / i), i);
+			break;
+		}
 	}
-	exit(EXIT_SUCCESS);
+}
+
+/**
+ * main - main
+ * @i: i
+ * @av: arg
+ * Return: return (0) on success
+ */
+
+int main(char **av)
+{
+    if (av[1] != NULL)
+	   file_input(av[1]);
+
+	return (0);
 }
